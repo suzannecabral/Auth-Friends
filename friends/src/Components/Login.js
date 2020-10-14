99f8d2305d1d2bbf6ffb69aa4691422afc536c62
Suzanne Cabral <suzannecabral@live.com>
2020-10-13 22:51:31 -0700

    Saving progress, next step see vid lecture, auth request on submit

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -13,11 +13,15 @@ const Login = () => {
         setCredentials({...credentials,[e.target.name]:e.target.value});
     };
 
+    const login = e => {
+        e.preventDefault();
+    };
+
 
     return(
         <div className="Login">
             <h2>Login:</h2>
-            <form>
+            <form onSubmit={login}>
                 <input type="text" name="username" placeholder="username" onChange={handleChange}/>
                 <input type="password" name="password" placeholder="password" onChange={handleChange}/>
                 <button>Sign in</button>
